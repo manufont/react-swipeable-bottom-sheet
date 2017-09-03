41f6e7f2a7fe5b255c0cdd79489004f0c21adcab
Emmanuel Fonteneau <emmanuel.fonteneau@eisti.fr>
2017-09-03 16:50:25 +0200

    Update 2017-09-03T14:50:23.156Z

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SwipeableBottomSheet = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = { "default": require("core-js/library/fn/object/assign"), __esModule: true };
 },{"core-js/library/fn/object/assign":15}],2:[function(require,module,exports){
 module.exports = { "default": require("core-js/library/fn/object/create"), __esModule: true };
@@ -3825,8 +3825,7 @@ if ("production" !== 'production') {
 
 module.exports = warning;
 
-},{}],114:[function(require,module,exports){
-(function (global){
+},{}],"react-swipeable-bottom-sheet":[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -3845,7 +3844,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _react = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
@@ -4007,6 +4006,4 @@ SwipeableBottomSheet.propTypes = {
 exports['default'] = SwipeableBottomSheet;
 module.exports = exports['default'];
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"prop-types":100,"react-swipeable-views":112}]},{},[114])(114)
-});
+},{"prop-types":100,"react":undefined,"react-swipeable-views":112}]},{},[]);
---
+++ fullscreen.js
@@ -0,0 +1,118 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _reactSwipeableBottomSheet = require('react-swipeable-bottom-sheet');
+
+var _reactSwipeableBottomSheet2 = _interopRequireDefault(_reactSwipeableBottomSheet);
+
+var App = (function (_React$Component) {
+	_inherits(App, _React$Component);
+
+	function App(props) {
+		_classCallCheck(this, App);
+
+		_get(Object.getPrototypeOf(App.prototype), 'constructor', this).call(this, props);
+
+		this.state = {
+			open: false
+		};
+	}
+
+	_createClass(App, [{
+		key: 'openBottomSheet',
+		value: function openBottomSheet(open) {
+			this.setState({ open: open });
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			var _this = this;
+
+			var styles = {
+				body: {
+					height: '100vh'
+				},
+				title: {
+					backgroundColor: '#3949ab',
+					padding: '16px 0',
+					boxSizing: 'border-box',
+					color: 'white',
+					height: '64px',
+					fontSize: '24px',
+					textAlign: 'center'
+				},
+				text: {
+					padding: '10px',
+					boxSizing: 'border-box',
+					backgroundColor: 'white',
+					fontSize: '18px'
+				}
+			};
+
+			return _react2['default'].createElement(
+				'div',
+				null,
+				_react2['default'].createElement(
+					'button',
+					{ onClick: function () {
+							return _this.openBottomSheet(true);
+						} },
+					'Open bottom sheet'
+				),
+				_react2['default'].createElement(
+					_reactSwipeableBottomSheet2['default'],
+					{
+						open: this.state.open,
+						onChange: this.openBottomSheet.bind(this),
+						bodyStyle: styles.body,
+						shadowTip: false,
+						topShadow: false },
+					_react2['default'].createElement(
+						'div',
+						{ style: styles.title },
+						'Controlled fullscreen bottom sheet'
+					),
+					_react2['default'].createElement(
+						'div',
+						{ style: styles.text },
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+						_react2['default'].createElement('br', null),
+						_react2['default'].createElement(
+							'button',
+							{ onClick: function () {
+									return _this.openBottomSheet(false);
+								} },
+							'close bottom sheet'
+						)
+					)
+				)
+			);
+		}
+	}]);
+
+	return App;
+})(_react2['default'].Component);
+
+;
+
+_reactDom2['default'].render(_react2['default'].createElement(App, null), document.getElementById('app'));
+
+},{"react":undefined,"react-dom":undefined,"react-swipeable-bottom-sheet":undefined}]},{},[1]);
